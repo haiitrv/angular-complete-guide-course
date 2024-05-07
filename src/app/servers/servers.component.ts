a5019b02467a5b8094effd45b37ce43001486ac5
HaiTrinh1511 <67938967+HaiTrinh1511@users.noreply.github.com>
2024-05-07 11:18:17 +0700

    Section 2 - Assignment 2-3

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,4 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.scss',
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  userName = '';
+  serverCreated = false;
+  servers = ['Server 1', 'Server 2'];
+  isDisplayedDetails = false;
+  logs = [];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 5000);
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdateUserName(event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
+
+  onAddUserName() {
+    this.userName = '';
+  }
+
+  toggleDisplay() {
+    this.isDisplayedDetails = !this.isDisplayedDetails;
+    this.logs.push(this.logs.length + 1);
+  }
+}
--- src/notes.md
+++ src/notes.md
@@ -14,3 +14,25 @@
 # AppModule
 
 - It is a bundle of functionalities of our app and it basically gives Angular information which features does my app have and use.
+
+# Data Binding
+
+- Communication
+
+- String Interpolation
+- Property Binding
+- Event Binding
+
+-> 2 way binding (combination) Ex: [(ngModel)]="data"
+
+# Property Binding vs String Interpolation
+
+- If we want to ouput something in the template such as print some text to it, use string interpolation.
+
+- If we want to change some property such as HTML element, use property binding.
+
+# Directives
+
+- The instructions in the DOM.
+
+- \*ng -> Structural directives: Change structure of the DOM by adding or changing it.
